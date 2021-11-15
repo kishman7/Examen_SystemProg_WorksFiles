9ad2c890054d52dc4a5b08df9c40f05eb29b820d
kishman7 <kishman_us@ukr.net>
2021-11-15 23:36:05 +0200

    Add Examen_SystemProg_WorksFiles

---
+++ WorkFile/MyLibFile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace WorkFile
+{
+    public class MyLibFile
+    {
+        public void CopyFile(string nameFile, string pathFile) //копіюємо файл
+        {
+            try
+            {
+                File.Copy(nameFile, pathFile);
+                Console.WriteLine("File copied successfully!!!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The file name or path is incorrect!!!");
+            }
+            //Console.WriteLine("CopyFile");
+        }
+
+        public void CopyDirectory(DirectoryInfo source, DirectoryInfo destination) //копіюємо директорію
+        {
+            try
+            {
+                if (!destination.Exists)
+                {
+                    destination.Create();
+                }
+                // Копіюємо всі файли.
+                FileInfo[] files = source.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    file.CopyTo(Path.Combine(destination.FullName,
+                        file.Name));
+                }
+                // Підкаталоги процесів.
+                DirectoryInfo[] dirs = source.GetDirectories();
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    // Отримати каталог призначення.
+                    string destinationDir = Path.Combine(destination.FullName, dir.Name);
+
+                    // Викликати CopyDirectory() рекурсивно.
+                    CopyDirectory(dir, new DirectoryInfo(destinationDir));
+                }
+
+                if (dirs != null)
+                {
+                    for (int i = 0; i < (dirs.Length - (dirs.Length - 1)); i++)
+                    {
+                        Console.WriteLine("Directory copied successfully!!!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Directory copied successfully!!!");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The directory name or path is incorrect!!!");
+            }
+        }
+        
+        public void DeleteFile(string nameFile) //видаляємо файл
+        {
+            try
+            {
+                File.Delete(nameFile);
+                Console.WriteLine("File deleted successfully!!!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The file name or path is incorrect!!!");
+            }
+        }
+
+        public void MoveFile(string path, string path2)
+        {
+            try
+            {
+                File.Move(path, path2);
+                Console.WriteLine("{0} was moved to {1}.", path, path2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The file name or path is incorrect!!!");
+            }
+
+        }
+    }
+}
---
+++ WorksInFiles/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using WorkFile;
+
+namespace WorksInFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibFile file = new MyLibFile();
+
+            bool action = true;
+            while (action)
+            {
+                Console.WriteLine("=====MENU=====");
+                Console.WriteLine("1.Copy File.");
+                Console.WriteLine("2.Copy Directory.");
+                Console.WriteLine("3.Delete File.");
+                Console.WriteLine("4.Move File.");
+                Console.WriteLine("0.Exit.");
+                Console.Write("Select a menu item: ");
+                int enter = Convert.ToInt32(Console.ReadLine());
+                switch (enter)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter a file name to copy: ");
+                            string nameFile = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this file: ");
+                            string pathFile = Console.ReadLine();
+                            string nameFilePath = pathFile + "\\" + nameFile; //шлях з назвою файлу, який копіюємо
+                            Console.WriteLine("Enter the name of the file you copied: ");
+                            string nameFileCopy = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this file: ");
+                            string pathFileCopy = Console.ReadLine();
+                            string nameFilePathCopy = pathFileCopy + "\\" + nameFileCopy; //шлях з назвою файлу, в який копіюємо
+                            file.CopyFile(nameFilePath, nameFilePathCopy); //копіюємо файл
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter a directory name to copy: ");
+                            string nameDirectory = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this directory: ");
+                            string pathDirectory = Console.ReadLine();
+                            string nameDirectoryPath = pathDirectory + "\\" + nameDirectory; //шлях з назвою директорії, яку копіюємо
+                            Console.WriteLine("Enter the name of the directory you copied: ");
+                            string nameDirectoryCopy = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this directory: ");
+                            string pathDirectoryCopy = Console.ReadLine();
+                            string nameDirectoryPathCopy = pathDirectoryCopy + "\\" + nameDirectoryCopy; //шлях з назвою директорії, в яку копіюємо
+
+                            DirectoryInfo sourceDir = new DirectoryInfo(nameDirectoryPath);
+                            DirectoryInfo destinationDir = new DirectoryInfo(nameDirectoryPathCopy);
+                            file.CopyDirectory(sourceDir, destinationDir); //копіюємо директорію
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter a file name to delete: ");
+                            string nameFile = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this file: ");
+                            string pathFile = Console.ReadLine();
+                            string nameFilePath = pathFile + "\\" + nameFile; //шлях з назвою файлу, який видаляємо
+
+                            file.DeleteFile(nameFilePath); //видаляємо файл
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Enter a file name to move: ");
+                            string nameFile = Console.ReadLine();
+                            Console.WriteLine("Enter the path to the folder: ");
+                            string pathFile = Console.ReadLine();
+                            string nameFilePath = pathFile + "\\" + nameFile; //шлях з назвою файлу та з якої папки переміщаємо
+                            Console.WriteLine("Enter the name of the file you moved: ");
+                            string nameFileMove = Console.ReadLine();
+                            Console.WriteLine("Enter the path of this file: ");
+                            string pathFileMove = Console.ReadLine();
+                            string nameFilePathMove = pathFileMove + "\\" + nameFileMove; //шлях з назвою файлу та в яку папку переміщаємо
+                            file.MoveFile(nameFilePath, nameFilePathMove); //переміщаємо файл
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 0:
+                        {
+                            action = false;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("The menu item entered is incorrect !!!");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+                
+        }
+    }
+}
